93db9a34e952600dc3e4b4c784e8c1e7312c5427
simmeringstew <dev.95a2e@simplelogin.com>
2022-11-28 10:18:35 -0500

    Drag and drop for vertical now working

--- src/createGameboard.js
+++ src/createGameboard.js
@@ -5,9 +5,9 @@ export default function createGameboard() {
         const yourRow = document.createElement("div");
         const firingRow = document.createElement("div");
         yourRow.classList.add("row");
-        yourRow.setAttribute("data-key", `${i}`);
+        yourRow.setAttribute("data-value", `${i}`);
         firingRow.classList.add("row");
-        firingRow.setAttribute("data-key", `${i}`);
+        firingRow.setAttribute("data-value", `${i}`);
         for (let j = 0; j < 10; j++) {
             const yourSquare = document.createElement("div");
             const firingSquare = document.createElement("div");
@@ -35,8 +35,8 @@ function getShipData(currentSquare) {
     const dragging = document.querySelector(".dragging");
     const length = parseInt(dragging.getAttribute("data-length"));
     const index = parseInt(currentSquare.getAttribute("data-key"));
+    const parentRow = currentSquare.parentNode;
     if (!dragging.classList.contains("vertical")) {
-        const parentRow = currentSquare.parentNode;
         try {
             if (length === 2) {
                 const neighbor = parentRow.querySelector(`[data-key="${index - 1}"]`);
@@ -56,7 +56,7 @@ function getShipData(currentSquare) {
                 squares = [secondPreviousNeighbor, previousNeighbor, currentSquare, nextNeighbor];
                 placeShips(squares, dragging);
             }
-            else if (length === 5) {
+            else {
                 const secondPreviousNeighbor = parentRow.querySelector(`[data-key="${index - 2}"]`);
                 const previousNeighbor = parentRow.querySelector(`[data-key="${index - 1}"]`);
                 const nextNeighbor = parentRow.querySelector(`[data-key="${index + 1}"]`);
@@ -72,7 +72,51 @@ function getShipData(currentSquare) {
         }
     }
     else {
-
+        try {
+            const shipBoard = document.querySelector(".your-ships");
+            const currentRowIndex = parseInt(parentRow.getAttribute("data-value"));
+            if (length === 2) {
+                const previousRow = shipBoard.querySelector(`[data-value="${currentRowIndex - 1}"]`);
+                const previousSquare = previousRow.querySelector(`[data-key="${index}"]`);
+                squares = [previousSquare, currentSquare];
+                placeShips(squares, dragging);
+            }
+            else if (length === 3) {
+                const previousRow = shipBoard.querySelector(`[data-value="${currentRowIndex - 1}"]`);
+                const nextRow = shipBoard.querySelector(`[data-value="${currentRowIndex + 1}"]`);
+                const previousSquare = previousRow.querySelector(`[data-key="${index}"]`);
+                const nextSquare = nextRow.querySelector(`[data-key="${index}"]`);
+                squares = [previousSquare, currentSquare, nextSquare];
+                placeShips(squares, dragging);
+            }
+            else if (length === 4) {
+                const secondPreviousRow = shipBoard.querySelector(`[data-value="${currentRowIndex - 2}"]`);
+                const previousRow = shipBoard.querySelector(`[data-value="${currentRowIndex - 1}"]`);
+                const nextRow = shipBoard.querySelector(`[data-value="${currentRowIndex + 1}"]`);
+                const secondPreviousSquare = secondPreviousRow.querySelector(`[data-key="${index}"]`);
+                const previousSquare = previousRow.querySelector(`[data-key="${index}"]`);
+                const nextSquare = nextRow.querySelector(`[data-key="${index}"]`);
+                squares = [secondPreviousSquare, previousSquare, currentSquare, nextSquare];
+                placeShips(squares, dragging);
+            }
+            else {
+                const secondPreviousRow = shipBoard.querySelector(`[data-value="${currentRowIndex - 2}"]`);
+                const previousRow = shipBoard.querySelector(`[data-value="${currentRowIndex - 1}"]`);
+                const nextRow = shipBoard.querySelector(`[data-value="${currentRowIndex + 1}"]`);
+                const secondNextRow = shipBoard.querySelector(`[data-value="${currentRowIndex + 2}"]`);
+                const secondPreviousSquare = secondPreviousRow.querySelector(`[data-key="${index}"]`);
+                const previousSquare = previousRow.querySelector(`[data-key="${index}"]`);
+                const nextSquare = nextRow.querySelector(`[data-key="${index}"]`);
+                const secondNextSquare = secondNextRow.querySelector(`[data-key="${index}"]`);
+                squares = [secondPreviousSquare, previousSquare, currentSquare, nextSquare, secondNextSquare];
+                placeShips(squares, dragging);
+            }
+        }
+        catch (e) {
+            if (e instanceof TypeError) {
+                return;
+            }
+        }
     }
 }
 
